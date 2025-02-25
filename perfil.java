8e14006fa18237c1d34f962b04b456123fcd67cf
brunosilva342 <brunoosilva9219@gmail.com>
2025-02-24 21:09:13 -0300

    Create perfil.java

---
+++ perfil.java
@@ -0,0 +1,14 @@
+public class Perfil {
+
+        public static void main(String[] args) {
+            // Definindo as variáveis
+            String nome = "Bruno Mikael Ribeiro da Silva";   
+            int idade = 27;         
+            String hobby = "Futebol"; 
+    
+            System.out.println("Nome: " + nome);
+            System.out.println("Idade: " + idade);
+            System.out.println("Hobby: " + hobby);
+        }
+    }
+    
